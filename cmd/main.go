f6c0e6616b244ab3d879ae6a5614379afb0dd106
Damien Sedgwick <damienksedgwick@gmail.com>
2024-04-07 19:26:24 +0100

    generated using napp

--- cmd/main.go
+++ cmd/main.go
@@ -5,150 +5,18 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"log"
-	"net/http"
+	"net/mail"
 	"os"
 	"time"
 
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
-
-func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("error loading godotenv")
-	}
-
-	r := http.NewServeMux()
-	fs := http.FileServer(http.Dir("./static"))
-	r.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	db, err := gorm.Open(sqlite.Open(os.Getenv("GODO_DB_PATH")), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(&User{}, &Todo{})
-
-	store := sessions.NewCookieStore([]byte(os.Getenv("GODO_COOKIE_STORE_SECRET")))
-
-	stack := CreateMiddlewareStack(
-		Logging,
-	)
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t := newTemplate()
-
-		session, _ := store.Get(r, "session")
-		if session.Values["user"] != nil {
-			var user User
-
-			err := json.Unmarshal(session.Values["user"].([]byte), &user)
-			if err != nil {
-				fmt.Println("error unmarshalling user value")
-			}
-
-			todos, err := GetAll(user.ID, db)
-			if err != nil {
-				fmt.Println("error getting users todos")
-			}
-
-			t.Render(w, "index", newPageData(user, todos))
-			return
-		}
-
-		var data interface{}
-
-		t.Render(w, "index", data)
-		return
-	})
-
-	r.HandleFunc("GET /auth/sign-in", func(w http.ResponseWriter, r *http.Request) {
-		t := newTemplate()
-
-		var data interface{}
-
-		t.Render(w, "auth-form", data)
-		return
-	})
-
-	r.HandleFunc("POST /auth/sign-in", func(w http.ResponseWriter, r *http.Request) {
-		t := newTemplate()
-
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Error parsing form data", http.StatusBadRequest)
-		}
-
-		email := r.Form.Get("email")
-		password := r.Form.Get("password")
-
-		var user User
-		db.First(&user, "email = ?", email)
-
-		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-			http.Error(w, "Unauthorised", http.StatusUnauthorized)
-			return
-		}
-
-		session, _ := store.Get(r, "session")
-		session.Options = &sessions.Options{
-			Path:     "/",
-			MaxAge:   86400 * 7,
-			HttpOnly: true,
-		}
-
-		userBytes, err := json.Marshal(user)
-		if err != nil {
-			fmt.Println("error marshalling user value")
-		}
-
-		session.Values["user"] = userBytes
-
-		err = session.Save(r, w)
-		if err != nil {
-			fmt.Println("error saving session: ", err)
-		}
-
-		todos, err := GetAll(user.ID, db)
-		if err != nil {
-			fmt.Println("error getting users todos")
-		}
-
-		t.Render(w, "index", newPageData(user, todos))
-		return
-	})
-
-	r.HandleFunc("POST /auth/sign-out", func(w http.ResponseWriter, r *http.Request) {
-		t := newTemplate()
-
-		sess, _ := store.Get(r, "session")
-		sess.Options.MaxAge = -1
-		err := sess.Save(r, w)
-		if err != nil {
-			fmt.Println("error saving session")
-		}
-
-		var data interface{}
-
-		t.Render(w, "index", data)
-		return
-	})
-
-	s := http.Server{
-		Addr:    ":8080",
-		Handler: stack(r),
-	}
-
-	fmt.Println("Running server on localhost:8080")
-	if err := s.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
-}
 
 type Template struct {
 	tmpl *template.Template
@@ -156,62 +24,143 @@ type Template struct {
 
 func newTemplate() *Template {
 	return &Template{
-		tmpl: template.Must(
-			template.ParseGlob("template/*.html")),
+		tmpl: template.Must(template.ParseGlob("template/*.html")),
 	}
 }
 
-func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
-type Middleware func(http.Handler) http.Handler
-
-func CreateMiddlewareStack(xs ...Middleware) Middleware {
-	return func(next http.Handler) http.Handler {
-		for i := len(xs) - 1; i >= 0; i-- {
-			x := xs[i]
-			next = x(next)
-		}
-		return next
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("error loading godotenv")
 	}
-}
 
-type wrappedWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
+	e := echo.New()
+	e.Renderer = newTemplate()
+	e.Static("/static", "static")
+	e.Use(middleware.Recover())
+	e.Use(middleware.Secure())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	store := sessions.NewCookieStore([]byte(os.Getenv("GODO_COOKIE_STORE_SECRET")))
+	e.Use(session.Middleware(store))
 
-func (w *wrappedWriter) WriteHeader(statusCode int) {
-	w.ResponseWriter.WriteHeader(statusCode)
-	w.statusCode = statusCode
-}
+	db, err := gorm.Open(sqlite.Open(os.Getenv("GODO_DB_PATH")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Lead{}, &User{})
 
-func Logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
+	e.GET("/", homepageHandler())
+	e.POST("/join-waitlist", joinWaitlistHandler(db))
 
-		wrapped := &wrappedWriter{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-
-		next.ServeHTTP(wrapped, r)
-
-		log.Println(r.Method, r.URL.Path, wrapped.statusCode, time.Since(start))
-	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 type PageData struct {
-	User  User
-	Todos []Todo
+	User     User
+	LeadForm LeadFormData
 }
 
-func newPageData(user User, todos []Todo) PageData {
+func newPageData(user User, leadForm LeadFormData) PageData {
 	return PageData{
-		User:  user,
-		Todos: todos,
+		User:     user,
+		LeadForm: leadForm,
 	}
+}
+
+func homepageHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, _ := session.Get("session", c)
+		if sess.Values["user"] != nil {
+			var user User
+			err := json.Unmarshal(sess.Values["user"].([]byte), &user)
+			if err != nil {
+				fmt.Println("error unmarshalling user value")
+				return err
+			}
+
+			return c.Render(200, "index", newPageData(user, newLeadFormData()))
+		}
+
+		return c.Render(200, "index", newPageData(newUser(), newLeadFormData()))
+	}
+}
+
+type Lead struct {
+	gorm.Model
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt *time.Time
+}
+
+type LeadFormData struct {
+	Errors map[string]string
+	Values map[string]string
+}
+
+func newLeadFormData() LeadFormData {
+	return LeadFormData{
+		Errors: map[string]string{},
+		Values: map[string]string{},
+	}
+}
+
+func joinWaitlistHandler(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		email := c.FormValue("email")
+		_, err := mail.ParseAddress(email)
+		if err != nil {
+			return c.Render(422, "waitlist", LeadFormData{
+				Errors: map[string]string{
+					"email": "Oops! That email appears to be invalid",
+				},
+				Values: map[string]string{
+					"email": email,
+				},
+			})
+		}
+
+		if leadExists(email, db) {
+			return c.Render(422, "waitlist", LeadFormData{
+				Errors: map[string]string{
+					"email": "Oops! It appears you are already subscribed",
+				},
+				Values: map[string]string{
+					"email": email,
+				},
+			})
+		}
+
+		lead := Lead{
+			Email: email,
+		}
+
+		if err := db.Create(&lead).Error; err != nil {
+			return c.Render(500, "waitlist", LeadFormData{
+				Errors: map[string]string{
+					"email": "Oops! It appears we have had an error",
+				},
+				Values: map[string]string{},
+			})
+		}
+
+		return c.Render(200, "waitlist", newLeadFormData())
+	}
+}
+
+func leadExists(email string, db *gorm.DB) bool {
+	var lead Lead
+	err := db.First(&lead, "email = ?", email).Error
+	if err == gorm.ErrRecordNotFound {
+		return false
+	}
+
+	return true
 }
 
 type User struct {
@@ -221,20 +170,8 @@ type User struct {
 	Password  string
 	CreatedAt time.Time
 	UpdatedAt *time.Time
-	Todos     []Todo
 }
 
-type Todo struct {
-	gorm.Model
-	Title       string
-	Completed   bool
-	Description *string
-	DueDate     *time.Time
-	UserId      uint
-}
-
-func GetAll(userId uint, db *gorm.DB) ([]Todo, error) {
-	var todos []Todo
-	err := db.Model(&Todo{}).Find(&todos).Where("id = ?", userId).Error
-	return todos, err
+func newUser() User {
+	return User{}
 }
